589d2e2cf44e1e4ba6ea91235f6567a10faa5f22
Marta Gómez <martagomezmartinez95@gmail.com>
2024-11-16 14:44:11 +0100

    Dashboard done

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import './App.css'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import RecipeList from './components/Recipes/RecipeList';
-import Sidebar from './components/Sidebar'
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
+import Sidebar from "./components/Sidebar";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-    return (
-        <>
-            <Navbar />
-            <body>
-              <RecipeList /> 
-            </body>
-            <Sidebar />
-        
-            <Footer />
-        </>
-        );
+  return (
+    <>
+      <Navbar />
+      <body>
+        <Dashboard />
+      </body>
+      <Sidebar />
+
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,14 @@
+import "./Dashboard.css"
+import RecipeList from "../components"
+
+
+function Dashboard(){
+
+    return(
+        <>
+    <RecipeList/>
+    </>
+    )
+}
+
+export default Dashboard
