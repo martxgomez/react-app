d47a1eaa8ba6ce699fae1704c0a15dda2ff68dd3
Andrea Bronzoni <bronzoni.andrea@libero.it>
2024-11-23 02:01:08 +0100

    Router, Routes and Route imported in main/App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import './App.css'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import RecipeList from './components/Recipes/RecipeList';
-import Sidebar from './components/Sidebar'
+import Sidebar from './components/Sidebar';
+import {Routes, Route} from 'react-router-dom';
 
 
 function App() {
