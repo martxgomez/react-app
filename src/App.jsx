fac783f80a17c1196d37c4aad660328d47ba9a45
Marta Gómez <martagomezmartinez95@gmail.com>
2024-11-30 22:26:27 +0100

    Dashboard, navbar, sidebar, footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import "./App.css";
 
 //data
-import recipesArray from "./data/recipes.json"
+import recipesArray from "./data/recipes.json";
 
 //react
 import { Routes, Route } from "react-router-dom";
@@ -24,37 +24,57 @@ import UpdateForm from "./components/Recipes/UpdateForm";
 function App() {
   const [recipes, setRecipes] = useState(recipesArray);
 
-
   function deleteRecipe(recipeId) {
-    const filteredRecipes = recipes.filter (recipe => {
-      return recipe.id !==recipeId;
+    const filteredRecipes = recipes.filter((recipe) => {
+      return recipe.id !== recipeId;
     });
     setRecipes(filteredRecipes);
   }
 
-  function updateRecipe (updatedRecipe) {
-    setRecipes((prevRecipes) => prevRecipes.map((recipe)=> recipe.id === updatedRecipe.id ? {...recipe, ...updatedRecipe}: recipe) )
+  function updateRecipe(updatedRecipe) {
+    setRecipes((prevRecipes) =>
+      prevRecipes.map((recipe) =>
+        recipe.id === updatedRecipe.id
+          ? { ...recipe, ...updatedRecipe }
+          : recipe
+      )
+    );
   }
 
   return (
-    <div >
+    <div>
       <Navbar />
-      <body>
-      <Sidebar />
-      <Routes>
-        <Route path="/" element={<DashboardPage recipes={recipes} deleteRecipe={deleteRecipe} />}  />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/form" element={<Form recipes={recipes} setRecipes={setRecipes} />} />
-        <Route path="/update/:recipeId" element={<UpdateForm recipes={recipes} setRecipes={setRecipes} updateRecipe={updateRecipe} />} />
-        <Route
-          path="/recipes/:recipeId"
-          element={<RecipeDetailsPage recipes={recipes}/>}
-    
-        />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-      </body>
-      
+      <section>
+        <Sidebar />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <DashboardPage recipes={recipes} deleteRecipe={deleteRecipe} />
+            }
+          />
+          <Route path="/about" element={<AboutPage />} />
+          <Route
+            path="/form"
+            element={<Form recipes={recipes} setRecipes={setRecipes} />}
+          />
+          <Route
+            path="/update/:recipeId"
+            element={
+              <UpdateForm
+                recipes={recipes}
+                setRecipes={setRecipes}
+                updateRecipe={updateRecipe}
+              />
+            }
+          />
+          <Route
+            path="/recipes/:recipeId"
+            element={<RecipeDetailsPage recipes={recipes} />}
+          />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </section>
 
       <Footer />
     </div>
--- src/components/Recipes/RecipeCard.jsx
+++ src/components/Recipes/RecipeCard.jsx
@@ -3,23 +3,30 @@ import { Link } from "react-router-dom";
 
 function RecipeCard({ id, name, calories, image, servings, deleteRecipe }) {
   return (
-    <>
-      <button onClick={() => deleteRecipe(id)} className="close">
-        ✖️
-      </button>
-      
-      <Link to={`/recipes/${id}`} className="recipe-card">
-        <li className="card">
-          <h3>{name}</h3>
-          <p>Calories: {calories} kcal {calories > 150 ? "👎🏻" : "👍🏻"}</p>
-          <img width="300" src={image} alt="recipe image" />
-          <p>Servings: {servings}</p>
-        </li>
+    <div className="card-container">
+      <Link to={`/update/${id}`} className="update-btn">
+        ✏️
       </Link>
 
-      {/* I add the button for the update form */}
-      <Link to={`/update/${id}`}><button>Update Recipe</button></Link>
-    </>
+      <button onClick={() => deleteRecipe(id)} className="close-btn">
+        ✖️
+      </button>
+
+      <Link to={`/recipes/${id}`} className="recipe-card">
+        <li className="card">
+          <div className="card-image">
+            <img src={image} alt={`${name}`} />
+          </div>
+          <div className="card-content">
+            <h3 className="card-title">{name}</h3>
+            <p className="card-servings">Servings: {servings}</p>
+            <p className="card-calories">
+              Calories: {calories} kcal {calories > 150 ? "👎🏻" : "👍🏻"}
+            </p>
+          </div>
+        </li>
+      </Link>
+    </div>
   );
 }
 
