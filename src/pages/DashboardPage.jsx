a9f9c57f46e3f6bfbb68539c4a7d1d061ac8837c
Marta Gómez <martagomezmartinez95@gmail.com>
2024-12-06 23:09:05 +0100

    Finished

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,10 +1,10 @@
 import RecipeList from "../components/Recipes/RecipeList";
 import "./DashboardPage.css";
 
-function DashboardPage({ recipes, deleteRecipe }) {
+function DashboardPage({ recipes, deleteRecipe, isOn}) {
   console.log(recipes);
   return (
-    <div className="dashboard-container">
+    <div className= {isOn ? "dashboard-container-on": "dashboard-container-off"} >
       <h2>Recipe List</h2>
       <RecipeList recipes={recipes} deleteRecipe={deleteRecipe} />
     </div>
